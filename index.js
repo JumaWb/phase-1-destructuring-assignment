1a5c48d3df9ce7ccf42fa1aca2a75d9cc4dd74b1
Brian Juma <bjuma0985@gmail.com>
2024-03-31 12:02:23 +0300

    using-array-reduce

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const animalSounds = {
+    cow: 'moo',
+    horse: 'neigh',
+    sheep: 'baa',
+    pig: 'oink',
+    chicken: 'cluck'
+}
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+// Assertions
+console.log(moo);    
+console.log(neigh);  
+console.log(baa);    
+console.log(oink);   
+console.log(cluck);  
+
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+// Assertions
+console.log(bessie);  // Output: cow
+console.log(dolly);   // Output: sheep
+console.log(babe);    // Output: pig
+console.log(little);  // Output: chicken
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+// Assertions
+console.log(blackAndWhite); // Output: cow
+console.log(black);          // Output: sheep
+console.log(pink);           // Output: pig
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// Destructuring to declare variables for each color
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+// Assertions for traditional rainbow color names
+console.log(red);   
+console.log(orange);  
+console.log(yellow); 
+console.log(green);  
+console.log(blue);    
+console.log(indigo); 
+console.log(violet);  
+
+// Destructuring using initials for six rainbow colors
+const [r, o, y, g, b, , v] = rainbowColors;
+
+// Assertions for rainbow color initials
+console.log(r);  
+console.log(o);  
+console.log(y);  
+console.log(g);  
+console.log(b);  
+console.log(v);  
+
+// Destructuring to declare Indigo using indg
+const [, , , , , indg] = rainbowColors;
+
+// Assertions for Indigo using indg
+console.log(indg);  // Output: indigo
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,6 +85,26 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+
+// Test case 1
+const { muppetName, color, song, job, partner } = muppet;
+
+// Assertions for test case 1
+console.log(muppetName); // Output: Miss Piggy
+console.log(color);      // Output: pink
+console.log(song);       // Output: Never Before, Never Again
+console.log(job);        // Output: Cast member of The Muppet Show
+console.log(partner);    // Output: Kermit
+
+// Test case 2
+const { songs: { song2, song4 }, nested: { job: nestedJob, partner: nestedPartner } } = muppet;
+
+// Assertions for test case 2
+console.log(song2);        // Output: Moving Right Along
+console.log(song4);        // Output: I Hope That Something Better Comes Along
+console.log(nestedJob);    // Output: Host of The Muppet Show
+console.log(nestedPartner);// Output: Miss Piggy
 
 // Strings
 
